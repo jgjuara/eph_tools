1015fbf42d531956331e6def889bfa38dfb79f16
jgjuara <jg.juara@gmail.com>
2021-06-21 17:59:22 -0300

    calculate_error corregido segun tidy style y según compatibilidad con paquete eph. Documentacion agregada. ScriptParaErrores corregido calculate_error segun tidy style y según compatibilidad con paquete eph. Falta correccion del resto de las funciones

---
+++ calculate_error.R
@@ -0,0 +1,58 @@
+#'Calculo del desvío estándar y el coeficiente de variación
+#'@description
+#'Funcion para asignar a una  estimación el desvío estándar y el coeficiente de variación
+#' correspondientes en base a la tabla de errores muestrales de INDEC.
+#'@param value Valor o vector de valores de las estimaciones puntuales para las
+#' que se desea hallar el desvío estándar o el coeficiente de variación
+#'@param codigo_aglo default = "Total". String con el código numerico de aglomerado que
+#' corresponde a la estimación o con "Total" para el total de 31 aglomerados urbanos (ver \code{\link{errores_muestrales}})
+#'@param measure default = "cv". String con "cv" para obtener el coeficiente de variación
+#' correspondiente a las estimaciones o con "ds" para obtener el desvío estándar. 
+#'@details disclaimer: El script no es un producto oficial de INDEC.
+#'
+#'@examples
+#'
+#' tabla <- eph::toybase_individual_2016_03 %>%
+#'   eph::organize_labels() %>%
+#'   eph::calculate_tabulates(x = "CH03",
+#'                          weights = "PONDERA",
+#'                          add.totals = "row")
+#'### hallar el coeficiente de variación de una lista de frecuencias
+#' calculate_error(tabla$Freq, measure = "cv")
+#'
+#'### hallar el desvio estándar para la columna de frecuencias
+#' tabla %>%
+#'   mutate(ds = calculate_error(Freq, measure = "ds"))
+#'
+#'### usando el parametro codigo_aglo
+#'
+#'tabla <- eph::toybase_individual_2016_03 %>%
+#'   eph::organize_labels() %>%
+#'   filter(AGLOMERADO == 32)
+#'   eph::calculate_tabulates(x = "CH03",
+#'                          weights = "PONDERA",
+#'                          add.totals = "row")
+#'tabla %>% 
+#'   mutate(ds = calculate_error(Freq, measure = "ds", codigo_aglo = "32"))
+
+#'@export
+
+
+calculate_error <- function(value, codigo_aglo = "Total", measure = "cv") {
+    #codigo_aglo = codigo de aglomerado en errores_muestrales
+    #medida = cv o ds según medida deseada
+    tabla_referencia <- eph::errores_muestrales %>%
+      dplyr::filter(aglomerado == codigo_aglo)  %>%
+      tidyr::pivot_longer(
+        cols = c("ds", "cv"),
+        names_to = "medida",
+        values_to = "valores"
+      ) %>%
+      dplyr::filter(medida == measure)
+    
+    find_closest <-function(y) {
+        tabla_referencia[["valores"]][which.min(abs(tabla_referencia[["x"]] - y))]
+      }
+    
+    sapply(value, find_closest)
+  }
--- scriptParaerroresEPH.R
+++ scriptParaerroresEPH.R
@@ -1,35 +1,62 @@
-closest_error_eph <- function(valor, aglomerado = 1, busqueda = "cv", periodo = 201403) {
-    #aglomerado = #lista de aglomerados
-    #busqueda = #CV o #DS
-    #periodo = #2003-2do trim 2014 ó #3er trim 2014 y posterior
-    tablaReferencia <- errores_eph %>% 
-        filter(codigo == aglomerado & periodo == periodo & variable == busqueda)
-    tablaReferencia[["value"]][which.min(abs(tablaReferencia[["estimacion"]]-valor))]
-}
-
-CVz <- function(CVx,CVy) round(sqrt((CVx)^2+(CVy)^2),2) # tal que Z = Y/X 
-#calculo el CV de la tasa como la raiz cuadrada de 
-# la suma de los cuadrados de los coeficientes de variacion de las estimaciones que componen la tasa
-# según el manual de error de la EPH.
-DSz <- function(z,cv) round(z*cv/100,2) # tal que cv sea el coeficiente de variacion de Z
-#calculo la DS de la tasa como el producto entre la tasa de actividad y su CV, redondeo a 2 digits
-
-# para toda tasa Z = Y/X*100
-tasasEPH <- function(num,den) {
-    cvx <- closest_error_eph(num)
-    cvy <- closest_error_eph(den)
-    z = 100*num/den
-    se <- DSz(z,CVz(cvx, cvy))
-    resultado <- list(tasa = z, stderror = se)
-    return(resultado)
-}
-
-limites <- function(tasasEPH, puntajez) {
-    Li = tasasEPH$tasa - tasasEPH$stderror*puntajez
-    Ls = tasasEPH$tasa + tasasEPH$stderror*puntajez
+calculate_error <- function(value, codigo_aglo = "Total", measure = "cv") {
+  #codigo_aglo = codigo de aglomerado en errores_muestrales
+  #medida = cv o ds según medida deseada
+  tabla_referencia <- errores_muestrales %>% 
+    filter(aglomerado == codigo_aglo)  %>% 
+    pivot_longer(cols = c("ds","cv"),
+                 names_to = "medida",
+                 values_to = "valores") %>% 
+    filter(medida == measure)
+      
+    find_closest <- function(y) {tabla_referencia[["valores"]][which.min(abs(tabla_referencia[["x"]]-y))]}
     
-    cat(paste(paste("Tasa Estimada =", round(tasasEPH$tasa,2)),
-              paste("Lim. Inf. = ",round(Li,2)),
-              paste("Lim. Sup. = ",round(Ls,2)),
-              sep = " \n "))
+    sapply(value, find_closest)
 }
+
+calculate_cv_rate <- function(cvx,cvy) {sqrt((cvx)^2 + (cvy)^2)} # tal que Z = X/Y 
+# calculo el CV de la tasa z como la raiz cuadrada de la suma de los cuadrados de 
+# los coeficientes de variacion de las estimaciones que componen la tasa
+# según el manual de error de la EPH.
+
+calculate_ds_rate <- function(z,cv) {z * cv / 100} # tal que cv sea el coeficiente de variacion de Z
+#calculo la DS de la tasa como el producto entre la tasa y su CV
+
+# para toda tasa Z = X/Y*100
+calculate_rate <- function(numerador,denominador) {
+  cvx <- calculate_error(numerador)
+  cvy <- calculate_error(denominador)
+  z <- 100 * numerador / denominador
+  se <- calculate_ds_rate(z,calculate_cv_rate(cvx, cvy))
+  
+  list(tasa = z, stderror = se)
+}
+
+limites <- function(tasasEPH, puntaje_z) {
+  Li = tasasEPH$tasa - tasasEPH$stderror*puntaje_z
+  Ls = tasasEPH$tasa + tasasEPH$stderror*puntaje_z
+  
+  cat(paste(paste("Tasa Estimada =", round(tasasEPH$tasa,2)),
+            paste("Lim. Inf. = ",round(Li,2)),
+            paste("Lim. Sup. = ",round(Ls,2)),
+            sep = " \n "))
+}
+
+
+# prueba con eph
+tabulados <- eph::toybase_individual_2016_03 %>%
+  eph::organize_labels() %>% 
+  eph:: calculate_tabulates(x = "CH03", weights = "PONDERA", add.totals = "row")
+
+calculate_error(tabulados$Freq, measure = "cv")
+
+tabulados %>% 
+  mutate(ds = calculate_error(Freq, measure = "ds"))
+
+eph::toybase_individual_2016_03 %>%
+   eph::organize_labels() %>%
+   filter(AGLOMERADO == 32) %>% 
+   eph::calculate_tabulates(x = "CH03",
+                          weights = "PONDERA",
+                          add.totals = "row") %>% 
+   mutate(ds = calculate_error(Freq, measure = "ds", codigo_aglo = "32"))
+
